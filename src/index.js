2e254bd7dc9a161f397ccf730d28570357153a6c
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2025-05-19 14:06:56 +0100

    cors error for http

--- src/index.js
+++ src/index.js
@@ -22,13 +22,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
 
+app.use(cors({ origin: true }));
 app.use("/api/auth", authRoutes);
 app.use("/api/property", propertiesRoutes);
 app.use("/api/agent", agentRoutes)
