f9e3ac3da9650fd9aa4e49a8b74178d52a3c518e
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2025-05-19 15:47:57 +0100

    cookie resolve

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,11 +3,11 @@ import jwt from 'jsonwebtoken'
 export const generateToken = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '3d' });
 
-    res.cookie('jwt', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production' ? true : false,
-        maxAge: 1000 * 60 * 60 * 24 * 3 ,// 3 days;,
-        sameSite: 'strict',
-    })
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
     return token;
 }
