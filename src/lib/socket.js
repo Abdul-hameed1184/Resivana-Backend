4f179e9f14c22f1b9fbde3763a40836b7adeb486
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2025-05-21 11:08:48 +0100

    url update cors for socket

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -8,8 +8,12 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
-    
+    origin: [
+      "http://localhost:5173",
+      "https://resivana.onrender.com",
+      "https://resivana-abdul-hameeds-projects-3a791a40.vercel.app",
+      "https://resivana.vercel.app",
+    ],
   },
 });
 
